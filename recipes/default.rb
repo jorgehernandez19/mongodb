cdb35822c11c3f576fd9e5b2fd2376c677260224
rene regalado <rene_regalado@epam.com>
2017-01-04 15:22:13 -0600

    Adding the mongodb cookbook changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,36 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#package 'epel-release' do
+#    action :install
+#end
+#
+#package 'nginx' do
+#    action :install
+#end
+#
+#service 'nginx' do
+#    action [ :enable, :start]
+#end
+#
+#directory '/usr/share/nginx/www/' do
+#    mode "0755"
+#
+if node['platform'] == 'centos'
+ # do ubuntu things
+ # :B
+   yum_repository 'mongodb' do
+      description "MongoDB repo ver 3.4"
+      baseurl "https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/"
+      gpgkey "https://www.mongodb.org/static/pgp/server-3.4.asc"
+      action :create
+   end
+end
+
+package 'mongodb-org' do
+    action :install
+end
+
+service 'mongod' do
+    action [ :enable, :start]
+end
